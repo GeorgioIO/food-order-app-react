aafa238aa74a48e493cb6fdbb51b8a3beac82178
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-05-02 13:19:10 +0300

    Fix quantity

--- src/components/Cart/CartButton.jsx
+++ src/components/Cart/CartButton.jsx
@@ -2,7 +2,7 @@ import { use } from "react";
 import { CartContext } from "../../store/cart-context";
 
 export default function CartButton({ onOpenModal }) {
-  const ctx = use(CartContext);
+  const cartCtx = use(CartContext);
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalCartItems + item.quantity;
